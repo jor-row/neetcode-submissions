b544c6ebad7a57f112082f4132e9011307190b7a
Jordan Rowan <86778628+jor-row@users.noreply.github.com>
2026-07-06 02:23:29 +0100

    Add: valid-word-abbreviation - submission-15

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-15.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        count = 0
+        currNum = ""
+
+        print(len(word))
+
+        if word == abbr:
+            return True
+
+        for s in abbr:
+            if s.isalpha():
+                count += 1
+                if currNum != "":
+                    print("adding, ", currNum)
+                    count += int(currNum)
+                    currNum = ""
+
+                if count > len(word) or s != word[count - 1]:
+                    return False
+                print(s)
+                print(word[count - 1])
+            elif s.isnumeric():
+                if currNum == "" and s == "0":
+                    return False
+                currNum += s
+
+        if currNum != "":
+            count += int(currNum)
+            currNum = ""
+
+        return count == len(word)
+        
