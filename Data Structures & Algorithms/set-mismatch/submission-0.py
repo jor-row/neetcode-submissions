238af5ff49777e61b33c378759178f97e3e76460
Jordan Rowan <86778628+jor-row@users.noreply.github.com>
2026-07-21 21:20:07 +0100

    Add: set-mismatch - submission-0

---
+++ Data Structures & Algorithms/set-mismatch/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        res = [0,0]
+        count = Counter(nums)
+
+        for i in range(1, len(nums)+1):
+            if i not in count:
+                res[1] = i
+            elif count[i] == 2:
+                res[0] = i
+
+        return res
+        
